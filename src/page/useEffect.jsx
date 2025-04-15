4e29e8087046e2d6f77953071a45dc13dd9366c3
윤종환 <yunjonghwan99@gmail.com>
2025-04-15 12:56:12 +0900

    함수 안에서 useState를 사용해야 에러가 발생을 하지 않음 만일 함수 외에 useState와 같은 훅을 사용하게 될 경우 함수형 컴포넌트 안에서만 사용할 수 있다고 에러가 발생함

--- src/page/useEffect.jsx
+++ src/page/useEffect.jsx
@@ -1,6 +1,8 @@
-const [count, setCount] = useState(0);
+import React, { useState } from 'react';
+
 
 function test() {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>Click Me ({count})</button>
