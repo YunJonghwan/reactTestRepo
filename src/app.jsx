dc2720c81d53e5662850f55ffd3bcc2a227f7528
윤종환 <yunjonghwan99@gmail.com>
2025-04-15 14:53:29 +0900

    하나의 파일을 계속 수정을 하면서 커밋을 진행하도록 변경

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
-import UseState1 from './page/useState1';
-import UseState2 from './page/useState2';
-import UseState3 from './page/useState3';
-import UseState4 from './page/useState4';
-import UseState5 from './page/useState5';
+import UseState from './page/useState';
 
 function Link() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/useState1" element={<UseState1 />} />
-          <Route path="/useState2" element={<UseState2 />} />
-          <Route path="/useState3" element={<UseState3 />} />
-          <Route path="/useState4" element={<UseState4 />} />
-          <Route path="/useState5" element={<UseState5 />} />
+          <Route path="/" element={<UseState />} />
         </Routes>
       </Router>
     </div>
