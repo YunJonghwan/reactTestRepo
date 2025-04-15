c34b2d8f0422861417e338bcec96d43fcb778974
윤종환 <yunjonghwan99@gmail.com>
2025-04-15 13:10:56 +0900

    안에 주석을 넣었더니 화면에 표시 되기 때문에 주석을 위로 옮김

--- src/page/useState3.jsx
+++ src/page/useState3.jsx
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 
 function useState2() {
   //* 기본값이 false로 지정된 boolean 타입의 변수
+  //* 버튼을 눌를 경우 버튼 이름 변경
+  //* 처음은 꺼짐으로 버튼을 누를 경우 반대의 값이 나옴
   const [isOn, setIsOn] = useState(false);
   return (
     <div>
-      //* 버튼을 눌를 경우 버튼 이름 변경
       <button onClick={() => setIsOn(!isOn)}>
-        //* 처음은 꺼짐으로 버튼을 누를 경우 반대의 값이 나옴
         {isOn ? "켜짐" : "꺼짐"}
       </button>
     </div>
