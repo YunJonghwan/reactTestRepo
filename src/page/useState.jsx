49e21d2fa2b15f896e88e4ba123f0e97c71e9da0
윤종환 <yunjonghwan99@gmail.com>
2025-04-15 15:29:44 +0900

    예제 8 : isVisible이 기본값으로 true가 정의 되어 있고 사용자가 토글을 누를때 isVisible의 값이 false로 변경되어 화면에 표시가 사라짐

--- src/page/useState.jsx
+++ src/page/useState.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 
 
 function useStateExample() {
-  const [randomNumber, setRandomNumber] = useState(() => Math.floor(Math.random() * 100));
+  const [isVisible, setIsVisible] = useState(true);
   return (
     <div>
-      {randomNumber}
+      {isVisible && <p>보이기 상태!</p>}
+      <button onClick={() => setIsVisible(!isVisible)}>토글</button>
     </div>
   )
 }
